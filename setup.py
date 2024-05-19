69617db6618d79851c391dc8c9ca5fbea5fa761c
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-05-19 11:44:14 +0800

    0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme_file:
 requirements = [] 
 setup(
     name="MailMagician",
-    version="0.2.0",
+    version="0.3.0",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package of email robots",
